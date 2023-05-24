fd38e5bbc3c7d053b757ac3aeff6593b84035067
A Stone <astone@heckgrammar.co.uk>
2023-05-24 09:24:43 +0100

    Add project files.

---
+++ NumberConversionsY9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberConversionsY9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int denNum;
+            int divider;
+            Console.Write("Enter the number you want to convert: ");
+            denNum = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number base you want to convert to: ");
+            divider = Convert.ToInt32(Console.ReadLine());
+            string binNum = "";
+            while (denNum > 0)
+            {
+                binNum = (denNum % divider).ToString() + binNum;
+                denNum = denNum / divider;
+            }
+            Console.WriteLine(binNum);
+
+
+        }
+    }
+}
